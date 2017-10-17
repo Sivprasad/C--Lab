740264c0ceac0258a3ed78aefe1f4ab737e5bdbc
Sivaprasad. G <sivaprasad.g@live.com>
2017-10-17 21:40:21 +0530

    No 12

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point();
+            Point bottomRIght = new Point(1366, 768);
+            double distance = origin.DistanceTo(bottomRIght);
+            Console.WriteLine($"Distance is : {distance}");
+            Console.WriteLine($"Number of Poin objexts: {Point.ObjectCount()} ");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
